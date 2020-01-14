fb7cc383bb5de1d8c40220d0f69cfe412949e206
Alex Malyutin <alex.malyutin.dev@gmail.com>
2020-01-14 12:27:24 +0300

    🎉 Initial commit.

---
+++ Editor/InterfaceWrapperDrawer.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace AlexMalyutinDev.InterfaceSerialization
+{
+	[CustomPropertyDrawer(typeof(InterfaceWrapper), true)]
+	public class InterfaceWrapperDrawer : PropertyDrawer
+	{
+		private List<Type> implementations;
+		private string[] implementationsNames;
+		private InterfaceWrapper wrapper;
+		private float height;
+
+		private bool cached = false;
+
+		//// Maybe someday it will work...
+		// public override VisualElement CreatePropertyGUI(SerializedProperty property)
+		// {
+		// 	Debug.Log("CreatePropertyGUI");
+		// 	var container = new VisualElement();
+
+		// 	var testField = new PropertyField(property.FindPropertyRelative("FullTypeName"));
+		// 	container.Add(testField);
+
+		// 	return container;
+		// }
+
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+		{
+			property.serializedObject.Update();
+
+			CacheImplementations(property);
+
+			position.height = EditorGUIUtility.singleLineHeight;
+			height = EditorGUIUtility.singleLineHeight;
+
+			EditorGUI.BeginChangeCheck();
+			var typeID = EditorGUI.Popup(position, property.displayName, wrapper.TypeID, implementationsNames);
+			Undo.RecordObject(property.serializedObject.targetObject, "popup");
+			if (EditorGUI.EndChangeCheck())
+			{
+				wrapper.TypeID = typeID;
+				wrapper.Type = implementations[wrapper.TypeID];
+				property.serializedObject.Update();
+				EditorUtility.SetDirty(property.serializedObject.targetObject);
+			}
+
+			position.x += 15;
+			position.width -= 15;
+			foreach (var field in wrapper.Type.GetFields())
+			{
+				height += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+				position.y += EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing;
+				if (field.FieldType == typeof(int))
+					field.SetValue(wrapper.Value, EditorGUI.IntField(position, field.Name, (int)field.GetValue(wrapper.Value)));
+			}
+
+			property.serializedObject.ApplyModifiedProperties();
+		}
+
+		private void CacheImplementations(SerializedProperty property)
+		{
+			if (!cached)
+			{
+				var propertyType = fieldInfo.FieldType;
+				var interfaceType = propertyType.BaseType.GetGenericArguments()[0];
+
+				implementations = propertyType.Assembly.GetTypes()
+					.Where(type => type.GetInterfaces().Contains(interfaceType)).ToList();
+				implementations.Insert(0, interfaceType);
+
+				implementationsNames = implementations.Select(type => type.IsInterface ? "<none>" : type.Name).ToArray();
+
+				wrapper = (InterfaceWrapper)GetValue(property);
+				try
+				{
+					wrapper.TypeID = implementations.FindIndex(type => type == wrapper.Type);
+				}
+				catch (Exception)
+				{
+					Debug.LogWarning("Type is missing!", property.serializedObject.targetObject);
+					wrapper.TypeID = 0;
+				}
+				cached = true;
+			}
+		}
+
+		// public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+		// {
+		// 	return height;
+		// }
+
+		public object GetValue(SerializedProperty property)
+		{
+			return fieldInfo.GetValue(property.serializedObject.targetObject);
+		}
+
+		public void SetPrpertyValue(SerializedProperty property, object value)
+		{
+			fieldInfo.SetValue(property.serializedObject.targetObject, value);
+		}
+	}
+}
---
+++ Runtime/InterfaceWrapper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+
+namespace AlexMalyutinDev.InterfaceSerialization
+{
+	[Serializable]
+	public class InterfaceWrapper
+	{
+		[SerializeField]
+		protected string FullTypeName;
+		[SerializeField]
+		protected string FullAssemblyName;
+		[SerializeField]
+		protected string JSON;
+
+		[SerializeField]
+		public int TypeID;
+
+		public virtual object Value { get; }
+		public virtual Type Type { get; set; }
+	}
+
+	[Serializable]
+	public class InterfaceWrapper<I> : InterfaceWrapper, ISerializationCallbackReceiver
+	{
+		internal InterfaceWrapper()
+		{
+			Type = typeof(I);
+		}
+
+		private I _value;
+		public override object Value => _value;
+
+		public override Type Type
+		{
+			get => !String.IsNullOrEmpty(FullTypeName) ?
+				Type.GetType($"{FullTypeName}, {FullAssemblyName}") ?? typeof(I) :
+				typeof(I);
+			set
+			{
+				FullTypeName = value.FullName;
+				FullAssemblyName = value.Assembly.FullName;
+				if (!value.IsInterface)
+					_value = (I)Activator.CreateInstance(value);
+			}
+		}
+
+		void ISerializationCallbackReceiver.OnBeforeSerialize()
+		{
+			if (!Type.IsInterface)
+				JSON = JsonUtility.ToJson(Convert.ChangeType(_value, Type));
+		}
+
+		void ISerializationCallbackReceiver.OnAfterDeserialize()
+		{
+			if (!Type.IsInterface)
+				_value = (I)JsonUtility.FromJson(JSON, Type);
+		}
+
+		public static explicit operator I(InterfaceWrapper<I> wrapper) => wrapper._value;
+	}
+}
